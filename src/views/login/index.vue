c2a33d1bfe14933a37a458d9dacd862eb53f600c
usamihal <usamihal001@163.com>
2021-07-31 14:30:23 +0800

    登录页UI界面构建

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,48 @@
 <template>
-  <div>登录页面</div>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-sm-9 col-md-7 col-lg-5 m-auto pt-5">
+        <div class="card mt-5">
+          <div class="card-header bg-white">
+            <h3 class="text-center mb-0 text-secondary">ADMIN</h3>
+          </div>
+          <div class="card-body">
+            <el-form ref="form" :model="form">
+              <el-form-item>
+                <el-input
+                  size="medium"
+                  v-model="form.username"
+                  placeholder="请输入用户名"
+                ></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-input
+                  size="medium"
+                  type="password"
+                  v-model="form.password"
+                  placeholder="请输入密码"
+                ></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button class="w-100" type="primary" size="medium">立即创建</el-button>
+              </el-form-item>
+            </el-form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      form: {}
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
