93c2a4dd6bd150c01e2a3bb3dda43563c47d01c6
usamihal <usamihal001@163.com>
2021-07-31 15:24:37 +0800

    后台主布局

--- src/App.vue
+++ src/App.vue
@@ -9,5 +9,19 @@ export default {
 }
 </script>
 <style lang="scss">
-
+::-webkit-scrollbar-track {
+  background: rgba(0, 0, 0, 0.1);
+  border-radius: 0;
+}
+::-webkit-scrollbar {
+  -webkit-appearance: none;
+  width: 10px;
+  height: 10px;
+}
+::-webkit-scrollbar-thumb {
+  cursor: pointer;
+  border-radius: 5px;
+  background: rgba(0, 0, 0, 0.25);
+  transition: color .2s ease;
+}
 </style>
--- src/views/layout.vue
+++ src/views/layout.vue
@@ -1,16 +1,39 @@
 <template>
   <div>
-    layout11
-    <router-view></router-view>
+    <el-container class="layout">
+      <el-header>Header</el-header>
+      <el-container class="layout-container">
+        <el-aside width="200px">
+          <ul>
+            <li v-for="i in 100" :key="i">{{ i }}</li>
+          </ul>
+        </el-aside>
+        <el-main>
+          <ul>
+            <li v-for="i in 100" :key="i">{{ i }}</li>
+          </ul>
+        </el-main>
+      </el-container>
+    </el-container>
+    <!-- <router-view></router-view> -->
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.layout {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  overflow: hidden;
+  .layout-container {
+    height: 100%;
+    padding-bottom: 60px;
+  }
+}
 </style>
